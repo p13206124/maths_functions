bd025e1934a2178417dc911402f0ed63bc08b065
Bartosz Wojcicki <P13206124@GH584-006.lec-admin.dmu.ac.uk>
2015-01-12 11:42:36 +0000

    average calculator

--- calc.c
+++ calc.c
@@ -3,10 +3,11 @@
 int main()
 {
 	int a, b, total;
+	int c, d, e, f, g, average;
 	char option;
 	
 	
-	printf("\navailable options (+), (-), (*), (/)\nselect option: ");
+	printf("\navailable options (+), (-), (*), (/), (a)verage\nselect option: ");
 	scanf("%c", &option);
 	printf("enter a: ");
 	scanf("%i", &a);
@@ -28,6 +29,20 @@ int main()
 		  break;
 	case '/': total = a / b;
 		  printf("%i\n", total);
+		  break;
+	case 'a': printf("Enter 5 numbers:\n");
+		  printf("Number 1: ");
+		  scanf("%i", &c);
+		  printf("Number 2: ");
+		  scanf("%i", &d);
+		  printf("Number 3: ");
+		  scanf("%i", &e);
+		  printf("Number 4: ");
+		  scanf("%i", &f);
+		  printf("Number 5: ");
+		  scanf("%i", &g);
+		  average = (c + d + e + f + g)/2;
+		  printf("\nAverage: %i\n", average);
 		  break;
 	default: printf("error\n");
 	}
