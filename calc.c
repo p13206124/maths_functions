cc963b060326f47fe91edad2d9356cc0e9410149
Bartosz Wojcicki <P13206124@GH584-006.lec-admin.dmu.ac.uk>
2015-01-19 11:50:40 +0000

    Added a menu function to display available options

--- calc.c
+++ calc.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+void menu()
+{
+	printf("\nAvailable options:\n");
+	printf("(+),\n");
+	printf("(-),\n");
+	printf("(*),\n");
+	printf("(/),\n");
+	printf("(a)verage\n");
+	printf("Select option: ");	
+}
+
 int main()
 {
 	int a, b, total;
 	int c, d, e, f, g, average;
 	char option;
 	
-	
-	printf("\navailable options (+), (-), (*), (/), (a)verage\nselect option: ");
+	menu();		//prints menu
 	scanf("%c", &option);
+	
 	printf("enter a: ");
 	scanf("%i", &a);
 	printf("enter b: ");
 	scanf("%i", &b);
-	
-
-
 	
 	switch(option){
 	case '+': total = a + b;
