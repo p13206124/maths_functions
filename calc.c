39239f74ba1a887b1f5fa97427bca2a50a43a141
Bartosz Wojcicki <P13206124@GH584-006.lec-admin.dmu.ac.uk>
2015-01-26 11:36:36 +0000

    Added a function that allows to enter 5 integers and return highest and lowest ones

--- calc.c
+++ calc.c
@@ -2,26 +2,29 @@
 
 int main()
 {
-	int a, b, total;
-	int c, d, e, f, g, average;
+	int a, b, total;	// + - * / functions variables
+	int c, d, e, f, g, average, min = 100000000, max = 0;	// average and minimum/maximum variales
+	int numbers = 0;
 	char option;
 	
 	
-	printf("\navailable options (+), (-), (*), (/), (a)verage\nselect option: ");
+	printf("\navailable options (+), (-), (*), (/), (a)verage, (m)aximum/minimum\nselect option: ");
 	scanf("%c", &option);
-	printf("enter a: ");
-	scanf("%i", &a);
-	printf("enter b: ");
-	scanf("%i", &b);
 	
-
-
 	
 	switch(option){
-	case '+': total = a + b;
+	case '+': printf("enter a: ");
+		  scanf("%i", &a);
+		  printf("enter b: ");
+		  scanf("%i", &b); 
+		  total = a + b;
 		  printf("%i\n", total);
 		  break;
-	case '-': total = a - b;
+	case '-': printf("enter a: ");
+		  scanf("%i", &a);
+		  printf("enter b: ");
+		  scanf("%i", &b);  
+		  total = a - b;
 		  printf("%i\n", total);
 		  break;
 	case '*': total = a * b;
@@ -30,7 +33,8 @@ int main()
 	case '/': total = a / b;
 		  printf("%i\n", total);
 		  break;
-	case 'a': printf("Enter 5 numbers:\n");
+	case 'a':
+	case 'A': printf("Enter 5 numbers:\n");
 		  printf("Number 1: ");
 		  scanf("%i", &c);
 		  printf("Number 2: ");
@@ -43,6 +47,25 @@ int main()
 		  scanf("%i", &g);
 		  average = (c + d + e + f + g)/5;
 		  printf("\nAverage is: %i\n", average);
+		  break;
+	case 'm':
+	case 'M': printf("Enter 5 integers: ");
+
+		  do{	  
+		      if (scanf(" %i", &a)==1){  
+		      	      if(a>max){	  
+		      	      max = a;
+		      	      }
+		  
+		      	      if(a<min){
+		      	          min = a;
+		      	      }
+		      	      numbers++;
+		      }
+
+		  } while(numbers!=5);
+		  
+		  printf("Your largest number was %i. Your smallest number was %i.\n", max, min);  
 		  break;
 	default: printf("error\n");
 	}
